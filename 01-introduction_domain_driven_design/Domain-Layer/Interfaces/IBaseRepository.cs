741cf1dc1c253202d6a2e38a47609ad942ac8e27
arthur-art <arthurteixeira.guts@gmail.com>
2023-09-17 21:17:25 -0300

    Trabalhando com domain driven design na modelagem do projeto, implementando entidades e interfaces da camada de dominio

---
+++ 01-introduction_domain_driven_design/Domain-Layer/Interfaces/IBaseRepository.cs
@@ -0,0 +1,22 @@
+﻿using Domain_Layer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain_Layer.Interfaces
+{
+    public interface IBaseRepository<TEntity> where TEntity : BaseEntitie
+    {
+        void Insert(TEntity obj);
+
+        void Update(TEntity obj);
+
+        void Delete(int id);
+
+        IList<TEntity> Select();
+
+        TEntity Select(int id);
+    }
+}
---
+++ 01-introduction_domain_driven_design/Domain-Layer/Interfaces/IBaseService.cs
@@ -0,0 +1,23 @@
+﻿using Domain_Layer.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain_Layer.Interfaces
+{
+    public interface IBaseService<TEntity> where TEntity : BaseEntity
+    {
+        TEntity Add<TValidator>(TEntity obj) where TValidator : AbstractValidator<TEntity>;
+
+        void Delete(int id);
+
+        IList<TEntity> Get();
+
+        TEntity GetById(int id);
+
+        TEntity Update<TValidator>(TEntity obj) where TValidator : AbstractValidator<TEntity>;
+    }
+}
